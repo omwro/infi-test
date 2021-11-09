a7c68ba9e92814801a8816b0aec36fdad0282b8d
Omer Erdem <omer.erdem@hva.nl>
2021-11-09 19:51:00 +0100

    CLI added

---
+++ camera.js
@@ -0,0 +1,14 @@
+class Camera {
+    constructor(id, camera, latitude, longitude) {
+        this.id = id;
+        this.camera = camera;
+        this.latitude = latitude
+        this.longitude = longitude
+    }
+}
+
+Camera.prototype.toString = function cameraToString() {
+    return `${this.id} | ${this.camera} | ${this.latitude} | ${this.longitude}`;
+};
+
+module.exports = Camera
